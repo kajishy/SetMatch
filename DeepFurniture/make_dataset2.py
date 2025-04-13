1b0d7747cf3ad0998d09d49429aefb48cbab5387
kajishy <kaji.yuki10@icloud.com>
2025-04-14 01:15:44 +0900

    Add make_test_example.py

---
+++ DeepFurniture/make_dataset2.py
@@ -0,0 +1,238 @@
+import tensorflow as tf
+import pickle
+import glob
+import numpy as np
+import pdb
+import os
+import sys
+
+def convert_label(label):
+    """
+    label が numpy 配列の場合、その中の値を取り出す。
+    すでにスカラーであればそのまま返す。
+    """
+    if isinstance(label, np.ndarray):
+        # 要素数が1なら、その要素を返す
+        if label.size == 1:
+            return label.item()
+        else:
+            # 複数要素の場合はリストに変換（用途に合わせて調整してください）
+            return label.tolist()
+    return label
+
+def ensure_numeric(labels):
+    new_labels = []
+    for lab in labels:
+        lab_conv = convert_label(lab)
+        # ここで文字列の場合に数値に変換（マッピング）
+        if isinstance(lab_conv, (str, np.str_)):
+            new_labels.append(lab_conv)
+        else:
+            new_labels.append(lab_conv)
+    return new_labels
+#-------------------------------
+class trainDataGenerator(tf.keras.utils.Sequence):
+    # base number of cameras
+    base_cam_num = 2
+
+    def __init__(self, year=2017, split=0, batch_size=20, max_item_num=8, max_data=np.inf):
+        data_path = f"pickle_data"
+        self.max_item_num = max_item_num
+        self.batch_size = batch_size
+        
+        # load train data
+        with open(f'{data_path}/train2.pkl', 'rb') as fp:
+            self.x_train = pickle.load(fp)
+            self.y_train = pickle.load(fp)
+        self.train_num = len(self.x_train)
+        # 各要素が numpy 配列の場合、スカラー文字列に変換
+        self.y_train = ensure_numeric(self.y_train)
+        # もし文字列ならユニークな値に基づいて数値ラベルに変換
+        if isinstance(self.y_train[0], (str, np.str_)):
+            unique = sorted(set(self.y_train))
+            self.label_map = {lab: i for i, lab in enumerate(unique)}
+            self.y_train = [self.label_map[label] for label in self.y_train]
+        
+
+        # limit data
+        if self.train_num > max_data:
+            self.train_num = max_data
+
+        # load validation data
+        with open(f'{data_path}/valid2.pkl', 'rb') as fp:
+            self.x_valid = pickle.load(fp)
+            self.y_valid = pickle.load(fp)
+        self.valid_num = len(self.x_valid)    
+        self.y_valid = ensure_numeric(self.y_valid)
+        if isinstance(self.y_valid[0], (str, np.str_)):
+            unique_val = sorted(set(self.y_valid))
+            self.label_map_val = {lab: i for i, lab in enumerate(unique_val)}
+            self.y_valid = [self.label_map_val[label] for label in self.y_valid]
+        
+        
+        # width and height of image
+        self.dim = len(self.x_train[0][0])
+
+        # shuffle index
+        self.inds = np.arange(len(self.x_train))
+        self.inds_shuffle = np.random.permutation(self.inds)
+
+    def __getitem__(self, index):
+        x, x_size, y = self.data_generation(self.x_tr, self.y_tr, self.inds_shuffle, index)
+        return (x, x_size), y
+
+    def data_generation(self, x, y, inds, index):
+        
+        if index >= 0:
+            # extract x and y
+            start_ind = index * self.batch_size
+            batch_inds = inds[start_ind:start_ind+self.batch_size]
+            x_tmp = [x[i] for i in batch_inds]
+            y_tmp = [y[i] for i in batch_inds]
+            batch_size = self.batch_size
+        else:
+            x_tmp = x
+            y_tmp = y
+            batch_size = len(x_tmp)
+
+        # split x
+        x_batch = []
+        x_size_batch = []
+        y_batch =[]
+        split_num = 2
+        for ind in range(batch_size):
+            x_tmp_split = np.array_split(x_tmp[ind][np.random.permutation(len(x_tmp[ind]))],split_num)
+            x_tmp_split_pad = [np.vstack([x, np.zeros([np.max([0,self.max_item_num-len(x)]),self.dim])])[:self.max_item_num] for x in x_tmp_split] # zero padding
+
+            x_batch.append(x_tmp_split_pad)
+            x_size_batch.append([len(x_tmp_split[i]) for i in range(split_num)])
+            y_batch.append(np.ones(split_num)*y_tmp[ind])
+
+        x_batch = np.vstack(x_batch)
+        x_size_batch = np.hstack(x_size_batch).astype(np.float32)
+        y_batch = np.hstack(y_batch)
+
+        return x_batch, x_size_batch, y_batch
+
+    def data_generation_val(self):
+        
+        x_val, x_size_val, y_val = self.data_generation(self.x_val, self.y_val, self.inds, -1)
+        return x_val, x_size_val, y_val
+
+    def __len__(self):
+        # number of batches in one epoch
+        batch_num = int(self.tr_num/self.batch_size)
+
+        return batch_num
+
+    def on_epoch_end(self):
+        # shuffle index
+        self.inds_shuffle = np.random.permutation(self.inds)
+#-------------------------------
+
+#-------------------------------
+class testDataGenerator(tf.keras.utils.Sequence):
+    def __init__(self, year=2017, split=0, cand_num=4):
+        self.data_path = f"data/{year}-{year}-split{split}"
+        self.cand_num = cand_num
+        # (number of groups in one batch) = (cand_num) + (one query)
+        self.batch_grp_num = cand_num + 1
+
+        # load data
+        with open(f'{self.data_path}/test_example_cand{self.cand_num}.pkl', 'rb') as fp:
+            self.x = pickle.load(fp)
+            self.x_size = pickle.load(fp)
+            self.y = pickle.load(fp)
+#-------------------------------
+
+#-------------------------------
+# gather and save pickle data
+def make_packed_pickle(files, save_path):
+    X = []
+    Y = []
+    for file in files:
+        with open(file, 'rb') as f:
+            try:
+                x = np.array(pickle.load(f))
+            except:
+                pass
+                #print(f"{os.path.basename(file)}: empty")
+            else:
+                #print(f"{os.path.basename(file)}: {x.shape}")
+                X.append(x)
+                Y.append(int(os.path.basename(file).split('.')[0]))
+
+    print(f"save to {save_path}")
+    with open(save_path,'wb') as f:
+        pickle.dump(X,f)
+        pickle.dump(Y,f)
+#-------------------------------
+
+#-------------------------------
+def make_packed_example_test_pickle(files, save_path, max_item_num):
+    X = []
+    X_size = []
+    Y = []
+    set_ids = []
+    image_ids = [] 
+    for file in files:
+        with open(file, 'rb') as f:
+            try:
+                x = pickle.load(f)
+                ids = pickle.load(f)
+            except:
+                pass
+            else:
+                dim = len(x[0][0])
+                stack = lambda z: np.vstack([np.vstack(z),np.zeros([np.max([0,max_item_num - len(z)]),dim])])[:max_item_num]
+                X.append(stack(x[0]))
+                X_size.append(np.float(len(x[0])))
+                [X.append(stack(x[1][i])) for i in range(len(x[1]))]
+                [X_size.append(np.float(len(x[1][i]))) for i in range(len(x[1]))]
+                Y.append(np.hstack([0,np.arange(len(x[1]))]))
+                set_ids.append(str(os.path.basename(file).split('.')[0]))
+                image_ids.append(ids[0])
+                image_ids.extend([i if isinstance(i[0], int) else i[0] for i in ids[1]])
+        
+    X = np.array(X)
+    X_size = np.array(X_size)
+    Y = np.hstack(Y)
+
+    with open(save_path,'wb') as f:
+        pickle.dump(X,f)
+        pickle.dump(X_size,f)
+        pickle.dump(Y,f)      
+        pickle.dump(set_ids,f)  
+        pickle.dump(image_ids,f) 
+    print("saving pickle files to " + str(save_path))
+#-------------------------------
+
+#-------------------------------        
+if __name__ == '__main__':   
+    # parameters
+    #year = int(sys.argv[1])
+    split = 0
+    max_item_num = 8
+    n_cands=5
+
+    # gen = testDataGenerator()
+
+    # path
+    data_path = f"pickle_data"
+    train_path = f"{data_path}/train"
+    test_path = f"{data_path}/test"
+    valid_path = f"{data_path}/valid"
+    test_example_path = f"json_data/pickles_plus/test_examples_ncomb_1_ncands_{n_cands}"
+
+    # file list
+    #train_files = glob.glob(f"{train_path}/*.pkl")
+    #test_files = glob.glob(f"{test_path}/*.pkl")
+    #valid_files = glob.glob(f"{valid_path}/*.pkl")
+    test_example_files = glob.glob(f"{test_example_path}/*.pkl")        
+    
+    # save packed pickle
+    #make_packed_pickle(train_files, f"{data_path}/train.pkl")
+    #make_packed_pickle(test_files, f"{data_path}/test.pkl")
+    #make_packed_pickle(valid_files, f"{data_path}/valid.pkl")
+    make_packed_example_test_pickle(test_example_files, f"{data_path}/test5_example_cand{n_cands}.pkl", max_item_num=max_item_num)
+#-------------------------------
---
+++ DeepFurniture/make_test_examples.py
@@ -0,0 +1,216 @@
+import numpy as np
+import glob
+import gzip
+import json
+import pickle
+import pathlib
+import tqdm
+from typing import Any, Dict, List, Optional, Tuple, Union
+import os
+
+def convert_ndarray_to_list(obj):
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()
+    elif isinstance(obj, dict):
+        return {k: convert_ndarray_to_list(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [convert_ndarray_to_list(item) for item in obj]
+    else:
+        return obj
+
+def get_test_data(label_dir: str) -> Tuple[List, List, List]:
+    path = pathlib.Path(label_dir)
+    test = json.load(open(path / "test.json"))
+    return test
+
+
+def get_labels(
+    year: Union[str, int], split: int, data_root: str,
+) -> List:
+    label_dir = f"{data_root}/journal/sameyears_v2/{year}-{year}-split{split}"
+    test = get_test_data(label_dir)
+
+    return test
+
+
+def load_feature(path: str):
+    with gzip.open(path, mode="rt", encoding="utf-8") as f:
+        feature = json.loads(f.read())
+    return feature
+
+
+def make_test_examples(
+    #test: List,
+    test_all: Tuple[List, List, List, List],#ラベルや画像IDなど
+    path: pathlib.Path,
+    n_comb: int = 1, 
+    n_cands: int = 4,
+    seed: int = 0,
+):
+    print("Make test dataset.")
+    test, test_y, test_z, test_id = test_all
+    np.random.seed(seed)
+
+    test_examples = []
+    for i in range(len(test)):
+        example = {}
+
+        lst = np.delete(np.arange(len(test)), i)
+        others = np.random.choice(lst, n_comb - 1, replace=False).tolist()
+        target = [i] + others
+
+        setX_items, setY_items = [], []
+        setX_ids, setY_ids = [], [] #image id
+        query_set_ids = [] #scene id
+        for j in target:
+            #items = [str(item["item_id"]) for item in test[j]["items"]]
+            items = test[j]
+            #items = np.array(items)
+            ids = test_id[j]
+
+            y_size = len(items) // 2
+
+            xy_mask = [True] * (len(items) - y_size) + [False] * y_size
+            xy_mask = np.random.permutation(xy_mask)
+            setX_items.extend(items[xy_mask].tolist())
+            setY_items.extend(items[~xy_mask].tolist())
+            setX_ids.extend(ids[xy_mask].tolist())
+            setY_ids.extend(ids[~xy_mask].tolist())
+            #query_set_ids.append(test[j]["set_id"])
+            query_set_ids.append(test_y[j])
+
+        example["query"] = setX_items
+        example["query_set_ids"] = query_set_ids
+        example["query_set_images_ids"] = setX_ids
+
+        answers = [setY_items]
+        answers_set_images_ids = [setY_ids]
+        answers_set_ids = [query_set_ids]
+        for j in range(n_cands - 1):
+            lst = np.delete(np.arange(len(test)), target)
+            negatives = np.random.choice(lst, n_comb, replace=False).tolist()
+            assert len(set(target) & set(negatives)) == 0
+            target += negatives  # avoid double-selecting
+
+            setY_items = []
+            setY_ids = [] #image id
+            setY_set_ids = [] #scene id
+            for k in negatives:
+                #items = [str(item["item_id"])for item in test[k]["items"]]
+                items = test[k]
+                ids = test_id[k]
+
+                """
+                items = np.random.permutation(items)
+                y_size = len(items) // 2
+                setY_items.extend(items[:y_size].tolist())
+                """
+                # インデックスを使って画像とIDを同じ順序で並び替える
+                perm_indices = np.random.permutation(items.shape[0])
+                items = items[perm_indices]
+                ids = ids[perm_indices]
+                y_size = len(items) // 2
+                setY_items.append(items[:y_size])
+                setY_ids.append(ids[:y_size])
+
+                setY_set_ids.append(test_y[k])
+            answers.append(setY_items)
+            answers_set_images_ids.append(setY_ids)
+            answers_set_ids.append(setY_set_ids)
+
+        example["answers"] = answers
+        example["answers_set_images_ids"] = answers_set_images_ids
+        example["answers_set_ids"] = answers_set_ids
+
+        test_examples.append(example)
+
+    with open(
+        path / f"test_examples_ncomb_{n_comb}_ncands_{n_cands}.json", "w"
+    ) as f:
+        json.dump(convert_ndarray_to_list(test_examples), f, indent=2)
+
+    return f"test_examples_ncomb_{n_comb}_ncands_{n_cands}"
+
+
+def save_test_examples(
+    json_path: pathlib.Path, output_dir: pathlib.Path,
+):
+
+    test_examples = json.load(open(json_path))
+    for i in tqdm.tqdm(range(len(test_examples))):
+        example = test_examples[i]
+        query = example["query"]
+        gallery = example["answers"]
+        #add image ids
+        query_ids = example["query_set_images_ids"]
+        gallery_ids = example["answers_set_images_ids"]
+
+        """
+        query_features = []
+        for item in query:
+            feat_name = str(item) + ".json.gz"
+            path = str(feature_dir / feat_name)
+            query_features.append(load_feature(path))
+
+        gallery_features = []
+        for set in gallery:
+            _feat = []
+            for item in set:
+                feat_name = str(item) + ".json.gz"
+                path = str(feature_dir / feat_name)
+                _feat.append(load_feature(path))
+            gallery_features.append(_feat)
+        """
+        query_features = query
+        gallery_features = gallery
+
+        example_features = [query_features, gallery_features]
+        example_ids = [query_ids, gallery_ids]
+        example_id = example["query_set_ids"][0]
+        with open(output_dir / f"{example_id}.pkl", "wb") as f:
+            pickle.dump(example_features, f)
+            pickle.dump(example_ids, f)
+
+    assert len(glob.glob(str(output_dir / "*"))) == len(test_examples), "unmatched case"
+
+    return
+
+
+def main(args):
+    # dataset
+    #test = get_labels(args.year, args.split, args.data_root)
+    label_dir = "pickle_data"
+    test_pkl = os.path.join(label_dir, "test2.pkl")
+    with open(test_pkl, "rb") as f:
+        test_x = pickle.load(f)
+        test_y = pickle.load(f)
+        test_z = pickle.load(f)
+        test_id = pickle.load(f)
+
+    #feature_dir = pathlib.Path(args.data_root) / "features"
+    #folder_name = f"{args.year}-{args.year}-split{args.split}"
+    output_root = pathlib.Path(args.data_root)# / "journal" / "test_examples" / folder_name
+    #output_pickles_root = output_root / "pickles"
+    output_root.mkdir(parents=True, exist_ok=True)
+    print("saving a json file to " + str(output_root))
+    json_name = make_test_examples((test_x, test_y, test_z, test_id), path=output_root, n_comb=1, n_cands=args.n_cands, seed=args.split)
+    json_path = output_root /  f"{json_name}.json" 
+    print("saved: " + str(json_path))
+
+    output_pickles_dir = output_root / "pickles_plus" / json_name
+    output_pickles_dir.mkdir(parents=True, exist_ok=True)
+    print("saving pickle files to " + str(output_pickles_dir))
+    save_test_examples(json_path, output_pickles_dir)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--split", type=int, choices=[0, 1, 2], default=0)
+    parser.add_argument("--data_root", type=str, default="json_data")
+    parser.add_argument("--n_cands", "-c", type=int, default=5)
+
+    args = parser.parse_args()
+
+    main(args)
